4f92b043b2e1bc82cb58d37044e0676fcc99d8db
imroner <imron.rosadi@yudharta.ac.id>
2020-10-21 21:03:27 +0700

    publish praktek oop pertama

---
+++ printprinln.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Moch Lutfi
+ */
+public class printprinln {
+    
+    public static void main(String[] args) {
+        System.out.println("I Love All (println)");
+        System.out.println("HUMANIKA JAYA");
+        System.out.print("humanika");
+        System.out.print(" jaya");
+    }
+}
