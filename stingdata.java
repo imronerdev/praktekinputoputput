50b9ed73892c98057035478080ca1b4be9e2dac0
imronerdev <imron.smkdt2011@gmail.com>
2020-11-04 14:03:32 +0700

    Revert "Revert "publish praktek oop pertama""
    This reverts commit 23f9b9e9b9f0d7146ee6a078389b37230bbadc8a.

---
+++ stingdata.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author Moch Lutfi
+ */
+public class stingdata {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nama");
+        System.out.print("Masukan nama pertama : ");
+        String namapertama = scanner.next();
+        System.out.print("Masukan nama kedua : ");
+        String namakedua=scanner.next();
+        //String result = value / anotherValue;
+        System.out.print("namapertama adalah : " + namapertama);
+       System.out.print("nama kedua adalah:"+namakedua);
+}}
---
+++ tipestring.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Moch Lutfi
+ */
+public class tipestring {
+      public static void main(String[] args) {
+    char[] dicodingChars = { 'I', 'L', 'O', 'V', 'E', '_', 'Y', 'O', 'U' };
+    String namaku="imronrosadi";
+    int umur=31;
+    String dicodingString = new String(dicodingChars);
+    System.out.println(dicodingString);
+    System.out.println("nama saya:"+namaku);
+    System.out.println("umur saya:"+umur);
+    int length = namaku.length();
+    System.out.println(length);
+    }  
+}
